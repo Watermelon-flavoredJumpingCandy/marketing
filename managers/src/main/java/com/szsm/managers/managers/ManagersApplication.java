ea1dd952ab8a3f7151ebd9586a78b88d7c70b222
Watermelon-flavoredJumpingCandy <50035669+Watermelon-flavoredJumpingCandy@users.noreply.github.com>
2022-09-15 08:04:01 +0800

    首次提交3

---
+++ managers/src/main/java/com/szsm/managers/managers/ManagersApplication.java
@@ -0,0 +1,13 @@
+package com.szsm.managers.managers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagersApplication.class, args);
+    }
+
+}
---
+++ managers/src/test/java/com/szsm/managers/managers/ManagersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szsm.managers.managers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
