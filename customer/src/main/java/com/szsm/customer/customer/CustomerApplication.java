f2de7f46fcc252fc18a6eb11261842fcbe6c73d4
Watermelon-flavoredJumpingCandy <1750745045@qq.com>
2022-09-22 10:51:36 +0800

    20220922--肖聪--添加注解：开启aop扫描及自动化配置

--- customer/src/main/java/com/szsm/customer/customer/CustomerApplication.java
+++ customer/src/main/java/com/szsm/customer/customer/CustomerApplication.java
@@ -1,9 +1,15 @@
 package com.szsm.customer.customer;
 
+import com.szsm.customer.customer.config.ShardeJedisPoolConfig;
+import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
+@EnableConfigurationProperties({ShardeJedisPoolConfig.class})
 @SpringBootApplication
 @MapperScan("com.szsm.customer.customer.mapper")
 public class CustomerApplication {
